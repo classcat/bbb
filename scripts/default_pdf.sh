b04643667395facd7145a7bbd8edb77f94b35e76
ClassCat <support@classcat.com>
2016-05-22 08:40:44 +0900

    pdf replaced

--- scripts/default_pdf.sh
+++ scripts/default_pdf.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# 22-may-16 : replace the pdf file.
+
 TARGET=/var/www/bigbluebutton-default
 
 # backup
 cp -p ${TARGET}/default.pdf ${TARGET}/default.pdf.orig
 
-
 install -v -o root -g root -m 0755 \
-    ../assets/ClassCatServiceCatalog-20150603.pdf \
+    ../assets/ClassCatCompany_Catalog-20160516.pdf \
     ${TARGET}/default.pdf
 
+#install -v -o root -g root -m 0755 \
+#    ../assets/ClassCatServiceCatalog-20150603.pdf \
+#    ${TARGET}/default.pdf
 
 exit 0
